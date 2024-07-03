cfe7d5414ef2876b88287636ebf9832fe77102fa
hoan.chuong <hoan.chuong@gotit.vn>
2024-07-03 14:16:32 +0700

    Khai báo connect db, config .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  Logger.log(`DB_HOST: ${configService.get<string>('DB_HOST')}`);
   await app.listen(3000);
 }
 bootstrap();
